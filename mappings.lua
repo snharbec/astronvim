b6122c43eb33606f07e75dbee0cd93afff1986a9
Stefan Harbeck <Stefan.Harbeck@saabgroup.com>
2023-07-19 18:30:44 +0200

    Update theme

--- mappings.lua
+++ mappings.lua
@@ -8,10 +8,14 @@ return {
   n = {
     -- second key is the lefthand side of the map
     -- mappings seen under group name "Buffer"
+    ["H"] = { "<cmd>bprev<cr>", desc = "Previous tab" },
+    ["L"] = { "<cmd>bnext<cr>", desc = "Previous tab" },
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(
+          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
+        )
       end,
       desc = "Pick to close",
     },
